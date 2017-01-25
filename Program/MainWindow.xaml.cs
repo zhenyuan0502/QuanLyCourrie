564d73f4581f51bb171a2037581a379865181800
NGUYEN.LT2192 <NGUYEN.LT2192@SINHVIEN.HOASEN.EDU.VN>
2017-01-26 04:33:05 +0700

    Add project files.

---
+++ Program/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace Program
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtUsername.Text == "" || txtPassword.Password == "")
+            {
+                MessageBox.Show("Tài khoản và mật khẩu không được để trống", "Đăng nhập thất bại");
+            }
+            else if(txtUsername.Text == "admin" && txtPassword.Password == "admin")
+            {
+                Management management = new Management();
+                management.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Sai tài khoản hoặc mật khẩu", "Đăng nhập thất bại");
+            }
+        }
+
+        private void titleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+    }
+}
