85f69907d0365236184ad36a130e8bdbec600176
NGUYEN.LT2192 <NGUYEN.LT2192@SINHVIEN.HOASEN.EDU.VN>
2017-07-14 15:30:52 +0700

    MVVM

--- Program/Properties/AssemblyInfo.cs
+++ Program/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Program")]
+[assembly: AssemblyTitle("Quản lý cửa hàng dây Courrie")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Program")]
+[assembly: AssemblyProduct("Courrie")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
